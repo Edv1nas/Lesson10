00777eb9d136c8bf4cf9d552b65efd87bea3747a
Edvinas Afarjancas <eafarjancas@gmail.com>
2023-02-15 18:43:44 +0200

    update

--- excercise_three.py
+++ excercise_three.py
@@ -1,19 +1,19 @@
 '''Create a mini python program which would take two numbers as an input and would return their sum, subtraction, division, multiplication.'''
 
-def num_addition(number1, number2):
+def num_addition(number1:int, number2:int) -> int:
     return number1 + number2
 
-def num_subtracion(number1, number2):
+def num_subtracion(number1:int, number2:int) -> int:
     return number1 - number2
 
-def num_division(number1, number2):
+def num_division(number1:int, number2:int) -> int:
     return number1 / number2
 
-def num_multiplication(number1, number2):
+def num_multiplication(number1:int, number2:int) -> int:
     return number1 * number2
 
 number1, number2 = int(input("Please input first number: ")), int(input("Please input second number: "))
 print(number1, "+", number2, "=", num_addition(number1, number2))
 print(number1, "-", number2, "=", num_subtracion(number1, number2))
-print(number1, "/", number2, "=", num_division(number1, number2))
+print(number1, "/", number2, "=", round(num_division(number1, number2,),2))
 print(number1, "*", number2, "=", num_multiplication(number1, number2))
